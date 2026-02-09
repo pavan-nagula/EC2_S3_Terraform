45881029c03c071cef39d58475477a77dfdeed51
Nagula Pavan <pavannagula2002@gmail.com>
2026-02-09 15:47:42 +0530

    Set default values for EC2 S3 instance variables
    Added default values for multiple variables in the EC2 S3 instance module.

--- modules/ec2_s3_instance/variables.tf
+++ modules/ec2_s3_instance/variables.tf
@@ -1,24 +1,30 @@
 variable "bucket_name" {
   type = string
+  default = "us-east-1"
 }
 
 variable "object_key_to_download" {
   type = string
+  default = "sample.txt"
 }
 
 variable "ami_id" {
   type = string
+  default = "ami-0532be01f26a3de55"
 }
 
 variable "instance_type" {
   type = string
+  default = "t2.nano"
 }
 
 variable "key_name" {
   type = string
+  default = "my-keypair"
 }
 
 variable "ssh_allowed_cidr" {
   type = string
+  default = "0.0.0.0/0"
 }
 
